9fcbdd78b035d450bb100421dc3f467699303c86
sgtsantiago <54191997+sgtsantiago@users.noreply.github.com>
2026-07-02 00:30:18 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.brasilnetworks.dryfire"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.brasilnetworks.dryfire"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+
+    val camerax = "1.5.1"
+    implementation("androidx.camera:camera-core:$camerax")
+    implementation("androidx.camera:camera-camera2:$camerax")
+    implementation("androidx.camera:camera-lifecycle:$camerax")
+    implementation("androidx.camera:camera-view:$camerax")
+}
